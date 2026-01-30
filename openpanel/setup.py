eedd126ed74acda85edd925b61ea4875cfc053ae
Dat Nguyen <ddatnguyen97@gmail.com>
2026-01-30 15:33:19 +0700

    Update openpanel setup

--- openpanel/setup.py
+++ openpanel/setup.py
@@ -5,27 +5,13 @@ def load_openpanel(client_id):
     st.markdown(
         f"""
         <script>
-          window.op = window.op || function(){{
-            var n = [];
-            return new Proxy(function(){{
-              arguments.length && n.push([].slice.call(arguments))
-            }}, {{
-              get: function(t, r) {{
-                return r === "q" ? n : function() {{
-                  n.push([r].concat([].slice.call(arguments)))
-                }}
-              }},
-              has: function(t, r) {{
-                return r === "q"
-              }}
-            }})
-          }}();
-          window.op('init', {{
+          window.op = window.op || [];
+          window.op.push(['init', {{
             clientId: '{client_id}',
             trackScreenViews: true,
             trackOutgoingLinks: true,
             trackAttributes: true,
-          }});
+          }}]);
         </script>
         <script src="https://openpanel.dev/op1.js" async></script>
         """,
