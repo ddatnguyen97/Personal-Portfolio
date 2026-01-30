18d09f1ba69de18f5751686293117830e9c00c59
Dat Nguyen <ddatnguyen97@gmail.com>
2026-01-30 14:57:40 +0700

    Update openpanel setup

--- openpanel/setup.py
+++ openpanel/setup.py
@@ -2,22 +2,32 @@ import streamlit.components.v1 as components
 import streamlit as st
 
 def load_openpanel(client_id):
-    components.html(
+    st.markdown(
         f"""
         <script>
-          window.op = window.op || function () {{
-            (op.q = op.q || []).push(arguments);
-          }};
-
+          window.op = window.op || function(){{
+            var n = [];
+            return new Proxy(function(){{
+              arguments.length && n.push([].slice.call(arguments))
+            }}, {{
+              get: function(t, r) {{
+                return r === "q" ? n : function() {{
+                  n.push([r].concat([].slice.call(arguments)))
+                }}
+              }},
+              has: function(t, r) {{
+                return r === "q"
+              }}
+            }})
+          }}();
           window.op('init', {{
-            clientId: "{client_id}",
-            trackScreenViews: false,
-            trackOutgoingLinks: false,
+            clientId: '{client_id}',
+            trackScreenViews: true,
+            trackOutgoingLinks: true,
             trackAttributes: true,
           }});
         </script>
-
         <script src="https://openpanel.dev/op1.js" async></script>
         """,
-        height=0,
+        unsafe_allow_html=True
     )
