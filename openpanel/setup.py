0289ac9f907ea0f4dc68c95f2ecf7bb2c50d0509
Dat Nguyen <ddatnguyen97@gmail.com>
2026-01-30 11:21:58 +0700

    Update openpanel setup

--- openpanel/setup.py
+++ openpanel/setup.py
@@ -2,7 +2,7 @@ import streamlit.components.v1 as components
 import streamlit as st
 
 def load_openpanel(client_id):
-    st.markdown(
+    components.html(
         f"""
         <script>
           window.op = window.op || function () {{
@@ -19,5 +19,5 @@ def load_openpanel(client_id):
 
         <script src="https://openpanel.dev/op1.js" async></script>
         """,
-        unsafe_allow_html=True
+        height=0,
     )
